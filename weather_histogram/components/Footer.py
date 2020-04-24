9fade2b505b96ae0599f1f0c2e4de8560571e9bf
Jan Macenka <36518907+jmacenka@users.noreply.github.com>
2020-04-24 14:10:01 +0200

    Added support for Repo-Link in Footer

--- weather_histogram/components/Footer.py
+++ weather_histogram/components/Footer.py
@@ -7,13 +7,27 @@ from random import randint
 def Footer(
     id:str='footer',
     footer_text:str=None,
+    footer_repo_link:str=None,
     className:str='text-muted text-center border-top pt-4',
     children:list=list(),
+    **kwargs,
 ):
+    
+    footer_children = [footer_text],
+    if not footer_repo_link is None:
+        footer_children.append(
+            dcc.Link(
+                id='github-repo-link',
+                href=footer_repo_link,
+                children=[
+                    'App source code',
+                    ],
+                )
+        )
+    footer_children.append(children)
+    
     return  html.Footer(
         id='footer-information',
         className=className,
-        children=[
-            footer_text,
-        ] + children
+        children=footer_children,
     )
